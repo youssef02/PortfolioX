218c9f2a874be7113803256051cb4bf988f30a0a
Youssef Arrassen <arrassen.youssef@gmail.com>
2024-06-15 14:44:38 +0100

    fix url 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ function Home() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await fetchFromPublic('/PortfolioX/data.json');
+        const data = await fetchFromPublic('PortfolioX/data.json');
         setCvData(data);
 
       } catch (error: any) {
